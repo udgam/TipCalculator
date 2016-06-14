e45c4a43506448b83a7034655ea14d4ab5b0a9e6
Udgam Goyal <udgam.goyal10@gmail.com>
2016-06-14 15:28:50 -0700

    Completed Required Materials for Tip calculator

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var billLabel: UITextField!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        
+        view.endEditing(true)
+        
+    }
 
+    @IBAction func calculateTip(sender: AnyObject) {
+        
+        let tipPercentages = [0.18,0.2,0.25]
+        
+        let bill = Double(billLabel.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        
+    }
 }
 
